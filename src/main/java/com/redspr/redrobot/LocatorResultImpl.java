fd1f791c57424dbf26a2393c8f1a85211c5f9423
Sam Hough <sam@redspr.com>
2013-06-13 19:15:39 +0100

    Javadoc

--- src/main/java/com/redspr/redrobot/LocatorResultImpl.java
+++ src/main/java/com/redspr/redrobot/LocatorResultImpl.java
@@ -1,5 +1,8 @@
 package com.redspr.redrobot;
 
+/**
+ * Simple implementation that wraps a native object.
+ */
 class LocatorResultImpl implements LocatorResult {
     private final Object wrapped;
 
